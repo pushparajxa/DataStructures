64c324dd0778383f63ef0fc01344ce069678568d
Pushparaj Motamari <pushparaj.motamari@rivigo.com>
2019-06-28 13:39:26 +0530

    Fixed small issue

--- src/main/java/com/dynamicProgramming/CoinChange.java
+++ src/main/java/com/dynamicProgramming/CoinChange.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class CoinChange {
 
   public static void main(String[] args) {
-    int [] input = {2,5,3,6};
+    int [] input = {6,3,5,2};
     int sum = 10;
 
     int result = getResult(input,sum);
