e94a3b79aac59c3b1f909579e505a7db4b02f22e
Pushparaj Motamari <pushparaj.motamari@rivigo.com>
2019-07-22 14:27:48 +0530

    Added code for finding the minimum number of columns to be deleted to make a list of strings sorted.

---
+++ src/main/java/com/lang/CharTest.java
@@ -0,0 +1,13 @@
+
+package com.lang;
+
+public class CharTest {
+
+  public static void main(String[] args) {
+    char c = 'a';
+    char d = 'd';
+    System.out.println((int)c);
+    System.out.println((int)d);
+    System.out.println(c>d);
+  }
+}
