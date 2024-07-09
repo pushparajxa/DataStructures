c24474ff52509ae55d5b13cbf8eb84a3caf4f798
Pushparaj Motamari <pushparajxa@gmail.com>
2024-07-09 00:05:28 -0700

    Fix bugs and add documentation to the code which finds strongly connected components

--- src/main/java/com/ds/graph/DirectedGraph/StronglyConnectedComponents.java
+++ src/main/java/com/ds/graph/DirectedGraph/StronglyConnectedComponents.java
@@ -2,7 +2,44 @@ package com.ds.graph.DirectedGraph;
 
 //https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
 //There are two algorithms to do this. 1.Tarjan Algorithm 2. Kosaraju Algorithm
+/*
 
+Strongly connected component: A component in which for any given vertex A to any vertex B, there
+will be paths in both the directions. It means there is a cycle among all the vertices of the
+component.
+
+This algorithm outputs strongly connected components in such a way that a vertex is part of only
+one component.
+
+
+This algorithm doesn't output all strongly connected components. For example below graph
+
+3
+4
+1 2
+2 1
+2 3
+3 1
+
+The output of above will be [1, 2, 3], but in fact you can have [1,2] and [1,2,3], i.e, two
+strongly connected components.
+
+
+
+Another example for testing this algorithm.
+6
+9
+2 1
+1 6
+5 6
+5 2
+6 2
+4 5
+5 4
+4 3
+3 2
+
+ */
 import com.ds.graph.utils.DirectedGraph;
 import com.ds.graph.utils.DirectedGraphUtils;
 
@@ -13,9 +50,10 @@ import static com.ds.graph.utils.DirectedGraph.*;
 public class StronglyConnectedComponents {
 
     public static void main(String[] args) {
-        DirectedGraph graph = DirectedGraphUtils.readDirectedGraph("/tmp/DirectedGraph");
+        DirectedGraph graph = DirectedGraphUtils.readDirectedGraph("/tmp/DirectedGraph3");
         Hashtable<Vertex,List<Vertex>> vertexListHashtable = tarjanAlgorithm(graph);
-        ArrayList<Map.Entry<Vertex,List<Vertex>>> entries = topologocalSort(graph);
+       // ArrayList<Map.Entry<Vertex,List<Vertex>>> entries = topologocalSort(graph);
+        System.out.println(vertexListHashtable.entrySet());
     }
 
     public static Hashtable<Vertex,List<Vertex>>  tarjanAlgorithm(DirectedGraph graph){
@@ -59,10 +97,10 @@ public class StronglyConnectedComponents {
         for(DirectedEdge edge: vertex.getOutEdges()){
             Vertex otherEnd = edge.getOtherEnd(vertex);
             if(!Vertex.isVisited(otherEnd)){
-                clock = findSCCs(otherEnd,clock,new Stack<>(),sccs);
+                clock = findSCCs(otherEnd,clock,stack,sccs);
                 int otherEndLowTime = Vertex.getLowTime(otherEnd);
                 lowTime = Math.min(lowTime,otherEndLowTime);
-            }else if(Vertex.getOnStackFlag(vertex)){
+            }else if(Vertex.getOnStackFlag(otherEnd)){
                 int otherEndDiscoveryTime = Vertex.getDiscoveryTime(otherEnd);
                 lowTime = Math.min(otherEndDiscoveryTime,lowTime);
             }else{
